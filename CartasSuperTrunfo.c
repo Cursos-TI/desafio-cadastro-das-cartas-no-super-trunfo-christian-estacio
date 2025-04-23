cb73ca532d7c713905fa1fb3b067ae46c7b02a5c
christian-estacio <christian@poavarejo.srv.br>
2025-04-23 01:46:23 +0000

    declaração de variaveis e captura de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,21 +4,21 @@ int main() {
 
     /* Cadastramento/declaração das variáveis para coleta dos dados das cartas */
     
-    char estado1[20], estado2[20]; /* neste campo o competidor deverá informar o nome dos estados */
-    char codigo1[3], codigo2[3]; /* neste campo o competidor deverá definir um código para cada carta */
-    char cidade1[20], cidade2[20]; /* neste campo o competidor deverá informar o nome das cidades */
-    int populacao1, populacao2; /* quantidade estimada de habitantes de cada cidade */
-    float area1, area2; /* tamanho da cidade em quilometros quadrados */
-    float pib1, pib2; /* produto interno bruto (total de riqueza) estimado de cada cidade*/
-    int pt1, pt2; /* quantidade  estimada de pontos turisticos de cada cidade */
+    char estado1,estado2; /* neste campo o competidor deverá informar uma letra entre 'A' e 'H' para cada estados */
+    char codigo1[3],codigo2[3]; /* neste campo o competidor deverá definir um código para cada carta */
+    char cidade1[20],cidade2[20]; /* neste campo o competidor deverá informar o nome das cidades */
+    int populacao1,populacao2; /* quantidade estimada de habitantes de cada cidade */
+    float area1,area2; /* tamanho da cidade em quilometros quadrados */
+    float pib1,pib2; /* produto interno bruto (total de riqueza) estimado de cada cidade*/
+    int pt1,pt2; /* quantidade  estimada de pontos turisticos de cada cidade */
 
     /* Obtenção e armazenamento das informações da carta 1*/
 
-    printf("Vamos começar o Super Trunfo?\nInforme o nome do primeiro estado:\n");
+    printf("Vamos começar o Super Trunfo?\nInforme uma letra entre A e H para o primeiro estado:\n");
     scanf("%s", &estado1);
 
-    printf("Informe um código para sua primeira carta entre '1' e '4':\n ");
-    scanf("%c", &codigo1);
+    printf("Informe um código para sua primeira carta entre '01' e '04':\n");
+    scanf("%s", &codigo1);
 
     printf("Informe o nome da primeira cidade:\n");
     scanf("%s", &cidade1);
@@ -27,21 +27,21 @@ int main() {
     scanf("%d", &populacao1);
 
     printf("Informe o tamnho da primeira cidade em km2:\n");
-    scanf("%s", area1);
+    scanf("%f", &area1);
 
     printf("Informe o PIB estimado da primeira cidade:\n");
-    scanf("%f", pib1);
+    scanf("%f", &pib1);
 
     printf("Informe o numero aproximado de pontos turisticos da primeira cidade\n");
-    scanf("%d", pt1);
+    scanf("%d", &pt1);
 
      /* Obtenção e armazenamento das informações da carta 2*/
 
-     printf("Vamos começar o Super Trunfo?\n Informe o nome do segundo estado:\n");
+     printf("Vamos começar o Super Trunfo?\n Informe uma letra entre A e H para o segundo estado:\n");
      scanf("%s", &estado2);
  
-     printf("Informe um código para sua segunda carta entre '5' e '8':\n ");
-     scanf("%c", &codigo2);
+     printf("Informe um código para sua segunda carta entre '01' e '04':\n ");
+     scanf("%s", &codigo2);
  
      printf("Informe o nome da segunda cidade:\n");
      scanf("%s", &cidade2);
@@ -50,23 +50,31 @@ int main() {
      scanf("%d", &populacao2);
  
      printf("Informe o tamnho da segunda cidade em km2:\n");
-     scanf("%s", area2);
+     scanf("%f", &area2);
  
      printf("Informe o PIB estimado da segunda cidade:\n");
-     scanf("%f", pib2);
+     scanf("%f", &pib2);
  
      printf("Informe o numero aproximado de pontos turisticos da segunda cidade\n");
-     scanf("%d", pt2);
+     scanf("%d", &pt2);
 
      /* Dados cadastrados - carta 1 e carta 2 */
 
-     printf("Código carta 1: %c Código carta 2: %c", codigo1, codigo2);
-     printf("Estado 1: %s Estado 2: %s", estado1, estado2);
-     printf("Cidade 1: %s Cidade 2: %s", cidade1, cidade2);
-     printf("População cidade 1: %d População cidade 2: %d", populacao1, populacao2);
-     printf("Tamanho cidade 1: %f Tamanho cidade 2: %f", area1, area2);
-     printf("PIB cidade 1: %f PIB cidade 2: %f", pib1, pib2);
-     printf("Nº de pontos turisticos cidade 1: %d Nº de pontos turisticos cidade 2: %d", pt1, pt2);
+     printf("Código carta 1: %s\n",&codigo1);
+     printf("Estado 1: %s\n",&estado1);
+     printf("Cidade 1: %s\n",&cidade1);
+     printf("População cidade 1: %d\n",&populacao1);
+     printf("Tamanho cidade 1: %f\n",&area1);
+     printf("PIB cidade 1: %f\n",&pib1);
+     printf("Nº de pontos turisticos cidade 1: %d\n",&pt1);
+
+     printf("Código carta 2: %s\n",&codigo2);
+     printf("Estado 2: %s\n",&estado2);
+     printf("Cidade 2: %s\n",&cidade2);
+     printf("População cidade 2: %d\n",&populacao2);
+     printf("Tamanho cidade 2: %f\n",&area2);
+     printf("PIB cidade 2: %f\n",&pib2);
+     printf("Nº de pontos turisticos cidade 2: %d\n",&pt2);
 
      return 0;
 
