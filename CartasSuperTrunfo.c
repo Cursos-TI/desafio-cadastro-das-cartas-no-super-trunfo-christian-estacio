24c4d22cab099783c4de7efb64fe957d1921b5ce
christian-estacio <christian@poavarejo.srv.br>
2025-04-24 13:31:33 +0000

    Revisão para correção de erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,87 +4,70 @@ int main() {
 
     /* Cadastramento/declaração das variáveis para coleta dos dados das cartas */
     
-    char estado1,estado2; /* neste campo o competidor deverá informar uma letra entre 'A' e 'H' para cada estados */
-    char codigo1[3],codigo2[3]; /* neste campo o competidor deverá definir um código para cada carta */
-    char cidade1[20],cidade2[20]; /* neste campo o competidor deverá informar o nome das cidades */
-    int populacao1,populacao2; /* quantidade estimada de habitantes de cada cidade */
-    float area1,area2; /* tamanho da cidade em quilometros quadrados */
-    float pib1,pib2; /* produto interno bruto (total de riqueza) estimado de cada cidade*/
-    int pt1,pt2; /* quantidade  estimada de pontos turisticos de cada cidade */
+    char codigo1[2],codigo2[2];
+    char estado1[2],estado2[2];
+    char cidade1[15],cidade2[15];
+    int populacao1,populacao2;
+    int pt1,pt2;
+            
+    /*Obtenção e armazenamento das informações da carta1*/
 
-    /* Obtenção e armazenamento das informações da carta 1*/
+    printf("Seja bem vindo ao Super Trunfo!\nVamos começar?\n\n");
 
-    printf("Vamos começar o Super Trunfo?\nInforme uma letra entre A e H para o primeiro estado:\n");
-    scanf("%s", &estado1);
+    printf("Informe uma número de 01 a 04 para o código primeira carta:\n");
+    scanf("%s",&codigo1);
 
-    printf("Informe um código para sua primeira carta entre '01' e '04':\n");
-    scanf("%s", &codigo1);
+    printf("Informe uma letra de A a D para o primeiro estado:\n");
+    scanf("%s",&estado1);
 
-    printf("Informe o nome da primeira cidade:\n");
-    scanf("%s", &cidade1);
+    printf("Informe uma cidade para primeira carta:\n");
+    scanf("%s",&cidade1);
 
-    printf("Informe a quantidade de habitantes da primeira cidade:\n");
-    scanf("%d", &populacao1);
+    printf("Informe a população da cidade1:\n");
+    scanf("%d",&populacao1);
 
-    printf("Informe o tamnho da primeira cidade em km2:\n");
-    scanf("%f", &area1);
+    printf("Informe o número de pontos turisticos da cidade1:\n");
+    scanf("%d",&pt1);
+    
 
-    printf("Informe o PIB estimado da primeira cidade:\n");
-    scanf("%f", &pib1);
+    /*Obtenção e armazenamento das informações da carta2*/
 
-    printf("Informe o numero aproximado de pontos turisticos da primeira cidade\n");
-    scanf("%d", &pt1);
+    printf("Informe uma número de 05 a 08 para o código da segunda carta:\n");
+    scanf("%s",&codigo2);
 
-     /* Obtenção e armazenamento das informações da carta 2*/
+    printf("Informe uma letra de E a F para o segundo estado:\n");
+    scanf("%s",&estado2);
 
-     printf("Vamos começar o Super Trunfo?\n Informe uma letra entre A e H para o segundo estado:\n");
-     scanf("%s", &estado2);
- 
-     printf("Informe um código para sua segunda carta entre '01' e '04':\n ");
-     scanf("%s", &codigo2);
- 
-     printf("Informe o nome da segunda cidade:\n");
-     scanf("%s", &cidade2);
- 
-     printf("Informe a quantidade de habitantes da segunda cidade:\n");
-     scanf("%d", &populacao2);
- 
-     printf("Informe o tamnho da segunda cidade em km2:\n");
-     scanf("%f", &area2);
- 
-     printf("Informe o PIB estimado da segunda cidade:\n");
-     scanf("%f", &pib2);
- 
-     printf("Informe o numero aproximado de pontos turisticos da segunda cidade\n");
-     scanf("%d", &pt2);
+    printf("Informe uma cidade para segunda carta:\n");
+    scanf("%s",&cidade2);
 
-     /* Dados cadastrados - carta 1 e carta 2 */
+    printf("Informe a população da cidade2:\n");
+    scanf("%d",&populacao2);
 
-     printf("Código carta 1: %s\n",&codigo1);
-     printf("Estado 1: %s\n",&estado1);
-     printf("Cidade 1: %s\n",&cidade1);
-     printf("População cidade 1: %d\n",&populacao1);
-     printf("Tamanho cidade 1: %f\n",&area1);
-     printf("PIB cidade 1: %f\n",&pib1);
-     printf("Nº de pontos turisticos cidade 1: %d\n",&pt1);
+    printf("Informe o número de pontos turisticos da cidade2:\n");
+    scanf("%d",&pt2);
 
-     printf("Código carta 2: %s\n",&codigo2);
-     printf("Estado 2: %s\n",&estado2);
-     printf("Cidade 2: %s\n",&cidade2);
-     printf("População cidade 2: %d\n",&populacao2);
-     printf("Tamanho cidade 2: %f\n",&area2);
-     printf("PIB cidade 2: %f\n",&pib2);
-     printf("Nº de pontos turisticos cidade 2: %d\n",&pt2);
+    /*Impressão dos dados coletados para carta1*/
 
-     return 0;
+    printf("\nCarta1\n");
 
- 
+    printf("Codigo1: %s\n",&codigo1);
+    printf("Estado1: %s\n",&estado1);
+    printf("Cidade1: %s\n",&cidade1);
+    printf("População1: %d\n",&populacao1);
+    printf("Pontos turisticos cidade1: %d\n",&pt1);
+    
+    /*Impressão dos dados coletados para carta2*/
 
+    printf("\nCarta2\n");
 
+    printf("Codigo2: %s\n",&codigo2);
+    printf("Estado2: %s\n",&estado2);
+    printf("Cidade2: %s\n",&cidade2);
+    printf("População2: %d\n",&populacao2);
+    printf("Pontos turisticos cidade2: %d\n",&pt2);
 
-
-
-
+    return 0;
 
 
 }
